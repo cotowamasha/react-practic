f96839c6ad5118a52f5714de2fc35f3198e328b7
Maria Kotova <cotowa.masha@mail.ru>
2021-03-24 15:30:07 +0300

    1 lrsson

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.css'
+
+class Messages extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: '',
+            messages: ['Hello', 'what\'s up?']
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value})
+    }
+    handleSubmit(event) {
+        this.state.messages.push(this.state.value)
+        this.setState({messages: this.state.messages})
+        this.setState({value: ''})
+        event.preventDefault()
+    }
+    render () {
+        return <div>
+            <h1>
+                Messages
+            </h1>
+            {this.state.messages.map(message => <p key={message}>{message}</p>)}
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    value={this.state.value}
+                    type="text"
+                    placeholder="Введите сообщение"
+                    onChange={this.handleChange}
+                />
+                <button>
+                    Отправить
+                </button>
+            </form>
+        </div>
+    }
+}
+
+ReactDOM.render(<Messages />, document.querySelector('#root'))
---
+++ webpack.config.js
@@ -0,0 +1,141 @@
+const path = require('path');
+
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+const getFileNameByEnv = (ext = '[ext]', name = '[name]') => isDevelopment ? `${name}.${ext}` : `${name}.[contenthash].${ext}`
+
+module.exports = {
+    mode: process.env.NODE_ENV || 'development',
+    context: path.resolve(__dirname, 'src'), 
+    entry: {
+         main: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        filename: getFileNameByEnv('js'),
+        path: path.resolve(__dirname, 'build')
+    },
+    optimization: isDevelopment ? {} : {
+        minimize: true,
+        minimizer: [new OptimizeCSSAssetsWebpackPlugin()]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+            filename: 'index.html',
+            inject: true,
+            minify: {
+                collapseWhitespace: !isDevelopment,
+                removeComments: !isDevelopment,
+                removeAttributeQuotes: !isDevelopment
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCSSExtractPlugin({
+            filename: `./css/${getFileNameByEnv('css')}`,
+            chunkFilename: getFileNameByEnv('css', '[id]')
+        }),
+        isDevelopment ? () => ({}) : new ImageMinimizerPlugin({
+            minimizerOptions: {
+                plugins: [
+                  ['gifsicle', { interlaced: true }],
+                  ['jpegtran', { progressive: true }],
+                  ['optipng', { optimizationLevel: 5 }],
+                  [
+                    'svgo',
+                    {
+                      plugins: [
+                        {
+                          removeViewBox: false,
+                        },
+                      ],
+                    },
+                  ],
+                ],
+              },
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.js(x?)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                exclude: [/node_modules/, /\.module\.css$/],
+                use: [isDevelopment ? 'style-loader' : {
+                    loader: MiniCSSExtractPlugin.loader,
+                    options: {
+                        publicPath: path.resolve(__dirname, 'build/css')
+                    }
+                },
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.module\.css$/,
+                exclude: [/node_modules/],
+                use: [isDevelopment ? 'style-loader' : {
+                    loader: MiniCSSExtractPlugin.loader,
+                    options: {
+                        publicPath: path.resolve(__dirname, 'build/css')
+                    }
+                },
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: isDevelopment,
+                        modules: true
+                    }
+                }
+                ]
+            },
+            {
+                test: /\.(?:|gif|png|jpg|jpeg|svg)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: `./img/${getFileNameByEnv()}`
+                    }
+                }]
+            },
+            {
+                test: /\.(?:|woff2)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: `./fonts/${getFileNameByEnv()}`
+                    }
+                }]
+            },
+        ]
+    },
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@app': path.resolve(__dirname, 'src/'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+        }
+    },
+    devServer: {
+        open: true,
+        hot: true,
+        port: 3000,
+        contentBase: path.resolve(__dirname, 'build'),
+    }
+}; 
